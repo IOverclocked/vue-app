12b085c1fccdaf471ab6404391d75383de11b348
Michal Osiatycki <michal.osiatycki@unitygroup.com>
2021-05-31 22:03:50 +0200

    watch

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,18 +1,34 @@
 <template>
+  <h2>{{ title }}</h2>
   <div>{{ quantity }}</div>
+  <div>{{ tax }}</div>
+  <button @click="add" >Add</button>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, reactive, ref, toRefs, watch } from 'vue'
 
 export default {
   name: 'MyComponent',
   setup() {
-    const quantity = ref(12)
+    const title = ref('MyComponent')
 
+    const state = reactive({
+      quantity: 0,
+      price: 100,
+      totalPrice: computed(() => state.price * state.quantity),
+      tax: computed(() => state.quantity * 0.23)
+    })
 
+    function add() {
+      state.quantity++
+    }
 
-    return { quantity }
+    watch(() => state.quantity, (prevQuantity, quantity) => {
+      console.log(prevQuantity, quantity)
+    })
+
+    return { title, add, ...toRefs(state) }
   }
 }
 </script>
