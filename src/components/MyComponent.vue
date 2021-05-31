1c1ee5b1609883964060a6db264033b747ee57cf
Michal Osiatycki <michal.osiatycki@unitygroup.com>
2021-05-31 21:39:50 +0200

    init

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ quantity }}</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'MyComponent',
+  setup() {
+    const quantity = ref(12)
+
+
+
+    return { quantity }
+  }
+}
+</script>
